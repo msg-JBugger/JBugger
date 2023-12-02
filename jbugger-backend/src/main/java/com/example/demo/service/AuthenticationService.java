5e5d5dc65898afb94caacb339ff80db083a48d9f
TufaruAlex <tufarualex3@gmail.com>
2023-12-02 17:01:30 +0200

    Field Validations

--- jbugger-backend/src/main/java/com/example/demo/service/AuthenticationService.java
+++ jbugger-backend/src/main/java/com/example/demo/service/AuthenticationService.java
@@ -26,22 +26,26 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        var user = User.builder()
-                .firstName(request.getFirstName())
-                .lastName(request.getLastName())
-                .mobileNumber(request.getMobileNumber())
-                .email(request.getEmail())
-                .username(generateUsername(request.getFirstName(), request.getLastName()))
-                .password(passwordEncoder.encode(generatePassword()))
-                .roles(setRoles(request.getRoles()))
-                .build();
-        userRepository.save(user);
-        var jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse
-                .builder()
-                .token(jwtToken)
-                .build();
+        if (checkPhoneNumberFormat(request.getMobileNumber()) && checkEmailFormat(request.getEmail())){
+            var user = User.builder()
+                    .firstName(request.getFirstName())
+                    .lastName(request.getLastName())
+                    .mobileNumber(request.getMobileNumber())
+                    .email(request.getEmail())
+                    .username(generateUsername(request.getFirstName(), request.getLastName()))
+                    .password(passwordEncoder.encode(generatePassword()))
+                    .roles(setRoles(request.getRoles()))
+                    .build();
+            userRepository.save(user);
+            var jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse
+                    .builder()
+                    .token(jwtToken)
+                    .build();
+        }
+        else throw new RuntimeException("Invalid Fields");
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
@@ -63,7 +67,7 @@ public class AuthenticationService {
 
     private Set<Role> setRoles(List<String> rolesData) {
         Set<Role> rolesSet = new HashSet<>();
-        for(String role: rolesData) {
+        for (String role : rolesData) {
             rolesSet.add(
                     roleRepository.findByType(RoleEnum.valueOf(role))
                             .orElseThrow(() -> new RuntimeException(role + " role not found"))
@@ -74,23 +78,23 @@ public class AuthenticationService {
 
     private String generateUsername(String firstName, String lastName) {
         StringBuilder username = new StringBuilder();
-        if(lastName.length() > 4) {
-            for(int i = 0; i < 5; i++) {
+        if (lastName.length() > 4) {
+            for (int i = 0; i < 5; i++) {
                 username.append(lastName.charAt(i));
             }
             username.append(firstName.charAt(0));
         } else {
-            for(int i = 0; i < lastName.length(); i++) {
+            for (int i = 0; i < lastName.length(); i++) {
                 username.append(lastName.charAt(i));
             }
-            for(int i = 0; i < 6-lastName.length(); i++) {
+            for (int i = 0; i < 6 - lastName.length(); i++) {
                 username.append(firstName.charAt(i));
             }
         }
         String usr = username.toString();
         usr = usr.toLowerCase();
         int k = 1;
-        while(!userRepository.findByUsername(usr).isEmpty()) {
+        while (!userRepository.findByUsername(usr).isEmpty()) {
             usr = usr.substring(0, 6) + k;
             k++;
         }
@@ -105,11 +109,29 @@ public class AuthenticationService {
                 "1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
         StringBuilder pass = new StringBuilder();
         Random rand = new Random();
-        for(int i = 0; i < 31; i++) {
+        for (int i = 0; i < 31; i++) {
             String randomElement = letters.get(rand.nextInt(letters.size()));
             pass.append(randomElement);
         }
         System.out.println(pass);
         return pass.toString();
+    }
+
+    private Boolean checkPhoneNumberFormat(String number) {
+        if (number.isEmpty()) return false;
+        int indexSize = 0;
+        if (number.startsWith("+49") || number.startsWith("+40"))
+            indexSize = 3;
+        else if (number.startsWith("0049") || number.startsWith("0040"))
+            indexSize = 4;
+        if (indexSize > 0) {
+            if (number.subSequence(indexSize, number.length()).length() <= 11) {
+                return true;
+            } else return false;
+        } else return false;
+    }
+
+    private Boolean checkEmailFormat(String email) {
+        return email.endsWith("@msggroup.com");
     }
 }
